1710ded06d6641a54db0823f7a335e33312332d8
Mark Helt <heltmm@gmail.com>
2017-12-08 11:14:19 -0800

    give total and items ids, remote true on delete link, respond to js file destroy, jquery remove item, update cart total and quanitity

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -23,7 +23,10 @@ class OrderItemsController < ApplicationController
     @item = @order.order_items.find(params[:id])
     @item.destroy
     @order.save
-    redirect_to cart_path
+    respond_to do |format|
+      format.html {redirect_to cart_path}
+      format.js { render "carts/destroy" }
+    end
   end
 
   private
