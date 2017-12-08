066f224256fea795451d71761f0dc91dc7706445
Mark Helt <heltmm@gmail.com>
2017-12-08 15:45:49 -0800

    update seed to add image, display image on page

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,13 +2,20 @@ class Seed
 
   def self.begin
     seed = Seed.new
+    seed.clear
     seed.generate_products
   end
 
   def generate_products
     20.times do |i|
-      Product.create!(name: Faker::Lorem.word, price: rand(10...100), description: Faker::Lorem.sentence(5, false, 0).chop)
+      Product.create!(name: Faker::Lorem.word, price: rand(10...100), description: Faker::Lorem.sentence(5, false, 0).chop, image: Faker::LoremPixel.image)
     end
+
+    p "Created #{Product.count} products"
+  end
+
+  def clear
+    Product.destroy_all
   end
 end
 
