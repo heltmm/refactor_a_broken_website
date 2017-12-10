455b4e8d537c20c7e14fa71eae0bfac7a29fbaef
Mark Helt <heltmm@gmail.com>
2017-12-10 12:01:16 -0800

    view allows decimals in price, model makers sure 0 or greater

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,4 +3,6 @@ class Product < ApplicationRecord
 
   has_attached_file :photo, :styles => { :large => "600x600", :medium => "300x300>", :thumb => "100x100#" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :photo, :content_type => /\Aimage\/.*\Z/
+
+  validates :price, :numericality => { :greater_than_or_equal_to => 0 }
 end
