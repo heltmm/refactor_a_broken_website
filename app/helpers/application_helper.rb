330cf5210b2337d3924725eefa863b53ab584f43
Mark Helt <heltmm@gmail.com>
2017-12-09 18:21:36 -0800

    add notices when sign in/out and registration, helpfer function to style messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def flash_class(level)
+    case level
+    when 'notice' then "card blue lighten-2 center-align"
+    when 'success' then "card blue lighten-2 center-align"
+    when 'error' then "card red light-4 center-align"
+    when 'alert' then "card red lighten-1 center-align"
+    end
+  end
 end
