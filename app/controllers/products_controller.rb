5a87df45b4a49b230dd11c9a6d016c2b089802a5
Mark Helt <heltmm@gmail.com>
2017-12-09 18:40:46 -0800

    edit, update, destroy in controller with flash notices for products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -17,10 +17,32 @@ class ProductsController < ApplicationController
   def create
     @product = Product.new(product_params)
     if @product.save
+      flash[:notice] = "Product successfully created!"
       redirect_to '/'
     else
       render :new
     end
+  end
+
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    if @product.update(product_params)
+      flash[:notice] = "Product successfully updated!"
+      redirect_to products_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+    flash[:notice] = "Product successfully deleted!"
+    redirect_to products_path
   end
 
   private
